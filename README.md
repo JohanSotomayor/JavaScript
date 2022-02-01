1f20b16ed1cfe7a423c36a8b50170ad3ee169984
Johan sotomayor <Johansvsr@gmail.com>
2022-02-01 17:03:36 -0500

    Primeros archivos de Javascript con estructura del lenguaje

---
+++ README.md
@@ -0,0 +1,12 @@
+# JAVASCRIPT DESDE 0
+## De: Johan Sotomayor {JSoto}
+
+- Bases del lenguaje
+- Termino principales (Scope, Hoisting, Coersion)
+- Variables (Declaracion, Inicializacion)
+- Tipado (Debil)
+- Condicionales
+- Loops
+- funciones
+- Arrays
+- Objetos
---
+++ funciones.js
@@ -0,0 +1,63 @@
+
+// // DEBILMENTE TIPADO
+// console.log(4+ "2")
+// console.log(4* "2")
+// console.log(4+true)
+// console.log(4-false)
+
+// //  /* COECION*/
+//  var a="2"
+//  var b=3
+//  var suma =a+b
+//  var coecion = Number(a)
+//  var sumabien = coecion+b
+//  console.log(suma)
+//  console.log(sumabien)
+ 
+
+
+// //  /* HOISTING*/
+// function suma(){
+//     var nombre= "Nicolas" + apellido
+//     return nombre
+// }
+
+// var apellido= "soto"
+// segundo= "johan"
+// console.log(suma() +segundo)
+
+// // Operador TERNARIO
+// numero=10
+
+// resultado= numero>8 ? "SI":"NO"
+// console.log(resultado)
+
+
+// ARRAYS
+
+var lista= ["Nicolas", "Alejo", "Camilo"]
+
+// lista.push("Melany") /*Añadir un objeto al final de la lista*/
+// lista.pop("Melany"); /*Borrar el ultimo objeto de la lista*/
+// lista.unshift("Janeth") /*Añadir un objeto al principio de la lista*/
+// lista.shift("") /*Borrar un objeto al principio de la lista*/
+// console.log(lista.indexOf("Nicolas")) /*Mapear ubicacion de un objeto dentro de la lista*/ 
+// console.log(lista.length)/* Saber longitud del array */
+
+// console.log(lista)
+// // LOOPS *CICLOS
+
+// for ( var i=0; i<lista.length; i++){
+//     console.log(lista[i])
+// }
+// // singular of plural
+// for (var hijo of lista){
+
+//     console.log("estos son mis " +hijo)
+// }
+
+
+// while(lista.length>0){
+//     console.log("hola " + lista.shift())
+// }
+
